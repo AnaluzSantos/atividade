fc823f81cda1083538b0d45d550a22c881762f90
Analuz Santos <108814723+AnaluzSantos@users.noreply.github.com>
2023-10-19 15:05:20 -0300

    Add files via upload

---
+++ recomposição/index.js
@@ -0,0 +1,27 @@
+// let variavel1 = "Variável Global"
+
+// {
+//     let variavel2 = "Variável Local"
+//     console.log(variavel1)
+//     console.log(variavel2)
+// }
+
+// console.log(variavel1)
+// console.log(variavel2)
+
+// Estrutura Condicional!
+// let idade = 18
+
+// if(18 === '18' && 15 == 15){
+//     console.log('Bloco 01')
+// }else if(5 >= 5 && 6<=6){
+//     console.log('Bloco 02')
+// }else{
+//     console.log('Bloco03')
+// }
+
+// Estrutura de Repetição
+for(let i = 1; i<= 10; i++){
+    console.log(i)
+}
+console.log(i)
